d3ae158512263c9d87cb5de396db9ecbfd880407
David Tejedor <david.tejedor@mo2o.com>
2024-08-07 20:34:24 +0200

    feat: fist commit

---
+++ MyHabitPal/MyHabitPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHabitPalApp.swift
+//  MyHabitPal
+//
+//  Created by David Tejedor on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MyHabitPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
